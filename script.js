e4fa412ee74c2624004292ceb9a8b3da06c028e7
Melissa Ahlstrom <melissa.ahlstrom@hotmail.com>
2020-06-05 19:35:10 -0400

    refactored html & added styles

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // DOCUMENT READY
 $(function() {
    
-   // user clicks button that scrolls them further down the page to set of inputs
+   // user begins by clicking button that scrolls them further down the page to set of inputs
    $('.scroll').on('click', function(e){
       e.preventDefault();
       $('html, body').animate({
@@ -14,7 +14,8 @@ $(function() {
    $('.create').on('click', function(e){
       e.preventDefault();
 
-      // getting the user inputted value using input id's and storing it in variables
+      // getting the user inputted value using input id's and storing it in variables with .val
+
       // Adjectives
       let userAdOne = $('#adOne').val();
       let userAdTwo = $('#adTwo').val();
@@ -37,28 +38,28 @@ $(function() {
       let userPlace = $('#place').val();
       let userPlant = $('#plant').val();
       let userBodyPart = $('#bodyPart').val();
-      let userNumber = $('number').val();
+      let userNumber = $('#number').val();
 
-      // creating array's to store all user inputted values
-      adjArray = new Array();
-      nounArray = new Array();
-      verbArray = new Array();
-      otherArray = new Array();
-
-      // push stored value from the variable adOne (that has adjectiveOne's inputted value) into the array
-      // Adjective Array
-      adjArray.push(userAdOne, userAdTwo, userAdThree);
-      // Noun Array
-      nounArray.push(userNounOne, userNounTwo, userNounThree, userPNounOne, userPNounTwo, userPNounThree, userPNounFour);
-      // Verb Array
-      verbArray.push(userVerbingOne, userVerbingTwo, userVerbingThree, userVerbingFour);
-      // Other Array
-      otherArray.push(userGame, userPlace, userPlant, userBodyPart, userNumber);
+      // input user information in a string
+      const story = `A vacation is when you take a trip to some ${userAdOne} place with your ${userAdTwo} family. Usually you go to some place that is near a/an ${userNounOne} or up to a/an ${userNounTwo}. A good vacation place is one where you can ride ${userPNounOne} or play ${userGame} or go hunting for ${userPNounTwo}. I like to spend my time ${userVerbingOne} or ${userVerbingTwo}. When parents go on a vacation, they spend their time eating three ${userPNounThree} a day, and father play golf, and mothers sit around ${userVerbingThree}. Last summer, my little brother fell in a/an ${userNounThree} and got poison ${userPlant} all over his ${userBodyPart}. My family is going to go to (the) ${userPlace}, and I will practice ${userVerbingFour}. Parents need vacations more than kids because parents are always very ${userAdThree} and because they have to work ${userNumber} hours every day all years making enough ${userPNounFour} to pay for the vacation.`;
+      
+      // display string on page
+      $('#storyText').text(story);
    })
-
-   // input user information in a string
-   // display string on page
-
-
 });
 
+// // creating array's to store all user inputted values
+// adjArray = new Array();
+// nounArray = new Array();
+// verbArray = new Array();
+// otherArray = new Array();
+
+// // push stored value from the variable adOne (that has adjectiveOne's inputted value) into the array
+// // Adjective Array
+// adjArray.push(userAdOne, userAdTwo, userAdThree);
+// // Noun Array
+// nounArray.push(userNounOne, userNounTwo, userNounThree, userPNounOne, userPNounTwo, userPNounThree, userPNounFour);
+// // Verb Array
+// verbArray.push(userVerbingOne, userVerbingTwo, userVerbingThree, userVerbingFour);
+// // Other Array
+// otherArray.push(userGame, userPlace, userPlant, userBodyPart, userNumber);
